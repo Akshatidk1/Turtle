225c4b85361f55a2c2aa7f3f85a7f72de4a749a9
Akshatidk1 <93459969+Akshatidk1@users.noreply.github.com>
2022-03-29 13:08:04 +0530

    Update demo_1.py

--- demo_1.py
+++ demo_1.py
@@ -21,7 +21,7 @@ def outline():
                                 thresholdType=cv2.THRESH_BINARY, blockSize=9, C=2)
     return th3
 
-image = 'alina.jpg'
+image = 'aaa.jpg' #image name which have to be in same folder
 im = cv2.imread(image, 0)
 th3 = outline()
 
